13cfb4af163e59d1fdd2e74db87427786e75a085
Federica Montesano <federica.montesano42@gmail.com>
2021-07-20 15:18:37 +0200

    Add files via upload
    sc_distributed_stack edited to support scal code

--- src/datastructures/sc_distributed_stack.h
+++ src/datastructures/sc_distributed_stack.h
@@ -4,30 +4,42 @@
 #include <atomic>
 #include <iostream>
 #include <vector>
+#include <mutex>
 
 #include "stack.h"
 #include "util/random.h"
 
 
+#include "datastructures/balancer.h"
+#include "datastructures/distributed_data_structure_interface.h"
+#include "datastructures/pool.h"
+
+
 namespace sds{
 
-    template<typename T, typename Stack>
-    class SCVectorStack {
+    template<typename T, typename Stack, typename Balancer>
+    class SCVectorStack : public Pool<T> {
     public:
-        SCVectorStack(uint64_t n, uint32_t k);
+        SCVectorStack(uint64_t n, uint32_t k, Balancer* balancer);
 
 	
 	    std::vector<Stack> S; //vector of stacks
 
         bool push(int index, T item);
-        bool pop(int index);
+        bool pop(int index, T item);
 	    int allocStack(int *val);
+
+        bool put(T item);
+        bool get(T *item);
+
 
 
     private:
         uint64_t n; //size
         uint32_t k; //stealing threshold
 	    int index;
+
+        Balancer* balancer_;
         
         //TODO: lock per il vettore di stack
         std::mutex mtx;
@@ -36,44 +48,47 @@ namespace sds{
 
     };
 
-    template<typename T, typename Stack>
-    SCVectorStack<T, Stack>::SCVectorStack(uint64_t size, uint32_t threshold) {
+    template<typename T, typename Stack, typename Balancer>
+    SCVectorStack<T, Stack, Balancer>::SCVectorStack(uint64_t size, uint32_t threshold, Balancer* balancer) {
+        
         n = size;
         k = threshold;
+        balancer_ = balancer;
 	    index = -1;
         S.resize(n);
     }
 
 
-    template<typename T, typename Stack>
+    /*template<typename T, typename Stack>
     int SCVectorStack<T, Stack>::allocStack(int *val) {
-	
+
 	   *val = ++index;
 
 	   return index;	
 	
-    }
+    }*/
 
 
-    template<typename T, typename Stack>
-    bool SCVectorStack<T, Stack>::push(int index, T item) {
-	    printf("SCVectorStack<T, Stack>::push on stack: %d of value: %d \n", index, item);
+
+    template<typename T, typename Stack, typename Balancer>
+    bool SCVectorStack<T, Stack, Balancer>::push(int index, T item) {
+	    printf("SCVectorStack<T, Stack>::push on stack: %d of value: %lu \n", index, item);
         return S.at(index).push(item);
 
     }
 
-    template<typename T, typename Stack>
-    bool SCVectorStack<T, Stack>::pop(int index) {
-    	T item;
+    template<typename T, typename Stack, typename Balancer>
+    bool SCVectorStack<T, Stack, Balancer>::pop(int index, T item) {
+    	//T item;
         bool res = S.at(index).pop(&item);
-        printf("SCVectorStack<T, Stack>::pop on stack: %d of value: %d \n", index, item);
+        printf("SCVectorStack<T, Stack>::pop on stack: %d of value: %lu \n", index, item);
         if (!res) { stealing(); }
         return true;
     }
 
 
-    template<typename T, typename Stack>
-    T SCVectorStack<T, Stack>::stealing() {
+    template<typename T, typename Stack, typename Balancer>
+    T SCVectorStack<T, Stack, Balancer>::stealing() {
         T item;
     	printf("SCVectorStack<T, Stack>::stealing()\n");
     	bool res;
@@ -86,6 +101,22 @@ namespace sds{
             //mtx.unlock();
             }
     	return item;
+    }
+
+
+
+    template<typename T, typename Stack, typename Balancer>
+    bool SCVectorStack<T, Stack, Balancer>::put(T item) {
+        const uint64_t i = balancer_->put_id();
+        return push(i, item);
+    }
+
+
+    template<typename T, typename Stack, typename Balancer>
+    bool SCVectorStack<T, Stack, Balancer>::get(T *item) {
+
+        uint64_t i = balancer_->get_id();
+        return pop(i, *item);
     }
 
 
