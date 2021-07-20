de5254a63a6fcf3e6be91a6853a5d9942b7e3f15
Federica Montesano <federica.montesano42@gmail.com>
2021-07-20 15:17:39 +0200

    Add files via upload
    main file for executing sc_distributed_stack
    to be edited --not working now

--- src/benchmark/std_glue/glue_sc_distributed_stack.cc
+++ src/benchmark/std_glue/glue_sc_distributed_stack.cc
@@ -1,12 +1,27 @@
 #include <inttypes.h>
 
+#include <stdio.h>
+#include <gflags/gflags.h>
+
 #include "benchmark/std_glue/std_pipe_api.h"
+#include "datastructures/balancer_1random.h"
 #include "datastructures/sc_stack.h"
 #include "datastructures/treiber_stack.h"
 #include "datastructures/sc_distributed_stack.h"
+#include "datastructures/distributed_data_structure.h"
+
+DEFINE_uint64(k, 100, "stealing threshold");
+
+DEFINE_bool(hw_random, false, "use hardware random generator instead "
+                              "of pseudo");
+#define GENERATE_BALANCER() (new scal::Balancer1Random(FLAGS_hw_random))
+
 
 void* ds_new() {
-  sds::SCVectorStack<uint64_t, scal::TreiberStack<uint64_t>> *vstack = new sds::SCVectorStack<uint64_t, scal::TreiberStack<uint64_t>>(5, 100);
+  sds::SCVectorStack<uint64_t, scal::TreiberStack<uint64_t>, scal::Balancer1Random> *vstack = new sds::SCVectorStack<uint64_t, scal::TreiberStack<uint64_t>, scal::Balancer1Random>(
+                                              g_num_threads+1, 100, GENERATE_BALANCER());
+  /*int index;
+  vstack->allocStack(&index);*/
   return static_cast<void*>(vstack);
 }
 
