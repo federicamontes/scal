4d0db03973d13b64466bdba11d4a749336f3f824
Federica Montesano <federica.montesano42@gmail.com>
2021-08-17 19:09:49 +0200

    Add files via upload

--- src/datastructures/sc_distributed_stack.h
+++ src/datastructures/sc_distributed_stack.h
@@ -6,117 +6,126 @@
 #include <vector>
 #include <mutex>
 
+
 #include "stack.h"
-#include "util/random.h"
-
-
-#include "datastructures/balancer.h"
-#include "datastructures/distributed_data_structure_interface.h"
 #include "datastructures/pool.h"
 
+namespace sds {
 
-namespace sds{
+    thread_local static int index;
 
-    template<typename T, typename Stack, typename Balancer>
+
+    template<typename T, typename Stack>
     class SCVectorStack : public Pool<T> {
     public:
-        SCVectorStack(uint64_t n, uint32_t k, Balancer* balancer);
+        SCVectorStack(uint64_t n, uint32_t k);
 
-	
 	    std::vector<Stack> S; //vector of stacks
 
-        bool push(int index, T item);
-        bool pop(int index, T item);
-	    int allocStack(int *val);
-
-        bool put(T item);
-        bool get(T *item);
-
+        bool put(T element);
+        bool get(T *element);
+        //void allocStack(int *val);
 
 
     private:
         uint64_t n; //size
         uint32_t k; //stealing threshold
-	    int index;
-
-        Balancer* balancer_;
+        uint64_t size_collision;
+        uint64_t delay;
+        int global_index;
         
+
         //TODO: lock per il vettore di stack
         std::mutex mtx;
+        std::mutex mtx_i; //mutex for incrementing index
 
+        
         T stealing();
+        bool push(int index, T element);
+        bool pop(int index, T element);
+        int retrieveStack(void);
 
     };
 
-    template<typename T, typename Stack, typename Balancer>
-    SCVectorStack<T, Stack, Balancer>::SCVectorStack(uint64_t size, uint32_t threshold, Balancer* balancer) {
+    template<typename T, typename Stack>
+    SCVectorStack<T, Stack>::SCVectorStack(uint64_t size, uint32_t threshold) {
         
         n = size;
         k = threshold;
-        balancer_ = balancer;
-	    index = -1;
         S.resize(n);
+        global_index = -1;
+    }
+
+
+
+    template<typename T, typename Stack>
+    int SCVectorStack<T, Stack>::retrieveStack() {
+
+       if (global_index == (int) n-1) global_index = -1;
+       index = ++global_index;
+
+       printf("index - global_index %d - %d\n", index, global_index);
+
+       return index;
+	
     }
 
 
     /*template<typename T, typename Stack>
-    int SCVectorStack<T, Stack>::allocStack(int *val) {
+    void SCVectorStack<T, Stack>::allocStack(int *val) {
 
-	   *val = ++index;
-
-	   return index;	
-	
+        mtx_i.lock();
+       *val = ++index;
+        mtx_i.unlock();
+    
     }*/
 
 
-
-    template<typename T, typename Stack, typename Balancer>
-    bool SCVectorStack<T, Stack, Balancer>::push(int index, T item) {
-	    printf("SCVectorStack<T, Stack>::push on stack: %d of value: %lu \n", index, item);
-        return S.at(index).push(item);
+    template<typename T, typename Stack>
+    bool SCVectorStack<T, Stack>::push(int index, T element) {
+	    printf("SCVectorStack<T, Stack>::push on stack: %d of value: %lu \n", index, element);
+        return S.at(index).push(element);
 
     }
 
-    template<typename T, typename Stack, typename Balancer>
-    bool SCVectorStack<T, Stack, Balancer>::pop(int index, T item) {
-    	//T item;
-        bool res = S.at(index).pop(&item);
-        printf("SCVectorStack<T, Stack>::pop on stack: %d of value: %lu \n", index, item);
+    template<typename T, typename Stack>
+    bool SCVectorStack<T, Stack>::pop(int index, T element) {
+        bool res = S.at(index).pop(&element);
+        printf("SCVectorStack<T, Stack>::pop on stack: %d of value: %lu \n", index, element);
         if (!res) { stealing(); }
         return true;
     }
 
 
-    template<typename T, typename Stack, typename Balancer>
-    T SCVectorStack<T, Stack, Balancer>::stealing() {
-        T item;
+    template<typename T, typename Stack>
+    T SCVectorStack<T, Stack>::stealing() {
+        T element;
     	printf("SCVectorStack<T, Stack>::stealing()\n");
     	bool res;
-            //TODO: lock
+            //TODO: lock for sc-stack
             //mtx.lock();
-            for (uint64_t i=0; i < this->n; i++) {
-                res = S.at(i).pop(&item);
+            for (uint64_t i=0; i < n; i++) {
+                res = S.at(i).pop(&element);
                 if (res) break;
             //TODO: unlock
             //mtx.unlock();
             }
-    	return item;
+    	return element;
     }
 
 
 
-    template<typename T, typename Stack, typename Balancer>
-    bool SCVectorStack<T, Stack, Balancer>::put(T item) {
-        const uint64_t i = balancer_->put_id();
-        return push(i, item);
+    template<typename T, typename Stack>
+    bool SCVectorStack<T, Stack>::put(T element) {
+        index = retrieveStack();
+        return push(index, element);
     }
 
 
-    template<typename T, typename Stack, typename Balancer>
-    bool SCVectorStack<T, Stack, Balancer>::get(T *item) {
-
-        uint64_t i = balancer_->get_id();
-        return pop(i, *item);
+    template<typename T, typename Stack>
+    bool SCVectorStack<T, Stack>::get(T *element) {
+        index = retrieveStack();
+        return pop(index, *element);
     }
 
 
