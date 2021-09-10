7e4277a33b5f0f55a053a19cb549f22779e5a48e
Federica Montesano <federica.montesano42@gmail.com>
2021-09-10 11:25:12 +0200

    Add files via upload
    bug corrected regarding instantiation of stacks

--- src/benchmark/std_glue/glue_sc_distributed_stack_ebstack.cc
+++ src/benchmark/std_glue/glue_sc_distributed_stack_ebstack.cc
@@ -9,14 +9,31 @@
 #include "datastructures/sc_distributed_stack.h"
 
 
-DEFINE_uint64(k, 100, "stealing threshold");
-
+//DEFINE_uint64(k, 100, "stealing threshold");
+DEFINE_uint64(collision, 0, "size of the collision array");
+DEFINE_uint64(delay, 15000, "time waiting in the collision array");
 
 
 void* ds_new() {
-  sds::SCVectorStack<uint64_t, scal::EliminationBackoffStack<uint64_t>> *vstack = new sds::SCVectorStack<uint64_t, scal::EliminationBackoffStack<uint64_t>>(
-                                              g_num_threads+1, 100);
-  
+
+  uint64_t size_collision = (g_num_threads + 1)/10;
+    if (FLAGS_collision != 0) {
+      size_collision = FLAGS_collision;
+    }
+    if (size_collision == 0) { 
+      size_collision = 1;
+  }
+  scal::EliminationBackoffStack<uint64_t> *ebs[g_num_threads+1];
+
+  sds::SCVectorStack<uint64_t, scal::EliminationBackoffStack<uint64_t>> *vstack = new sds::SCVectorStack<uint64_t,
+                              scal::EliminationBackoffStack<uint64_t>>(g_num_threads+1, g_threshold, g_operations);
+
+
+  for(uint i=0; i <= g_num_threads; i++) {
+    ebs[i] = new scal::EliminationBackoffStack<uint64_t>(g_num_threads + 1, 
+          size_collision, FLAGS_delay);
+    vstack->S.at(i) = ebs[i];
+  }
 
   return static_cast<void*>(vstack);
 }
--- src/benchmark/std_glue/glue_sc_distributed_stack_treiber.cc
+++ src/benchmark/std_glue/glue_sc_distributed_stack_treiber.cc
@@ -9,14 +9,21 @@
 #include "datastructures/sc_distributed_stack.h"
 
 
-DEFINE_uint64(k, 100, "stealing threshold");
 
+//DEFINE_uint64(k, 10, "stealing threshold");
 
 
 void* ds_new() {
+
+  scal::TreiberStack<uint64_t> *ts[g_num_threads+1];
   sds::SCVectorStack<uint64_t, scal::TreiberStack<uint64_t>> *vstack = new sds::SCVectorStack<uint64_t, scal::TreiberStack<uint64_t>>(
-                                              g_num_threads+1, 100);
+                                              g_num_threads+1, g_threshold, g_operations);
   
+  for(uint i=0; i <= g_num_threads; i++) {
+    ts[i] = new scal::TreiberStack<uint64_t>();
+    vstack->S.at(i) = ts[i];
+    //printf("vstack->S.at(i) %p\n", vstack->S.at(i));
+  }
 
   return static_cast<void*>(vstack);
 }
