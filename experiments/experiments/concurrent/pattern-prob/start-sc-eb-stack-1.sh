d17a8af08e1dbdf13312bf876e2413cc4d2c509a
Federica Montesano <federica.montesano42@gmail.com>
2021-09-20 10:43:13 +0200

    Add files via upload

--- experiments/experiments/concurrent/pattern-prob/start-sc-eb-stack-1.sh
+++ experiments/experiments/concurrent/pattern-prob/start-sc-eb-stack-1.sh
@@ -5,7 +5,12 @@
 
 cd ..; cd ..; cd ..; cd out/Debug;
 
-count=10
-for i in $(seq $count); do
-    ./concurrent-sc-eb-stack -num_threads=$1 -T=$2 -access_pattern=1 -avg_size=$3 >> ./output/sc-eb-stack$4 ;
+declare -a arr=(1 2 5 10 20 40 60 80)
+
+
+count=3
+for j in "${arr[@]}"; do
+	for i in $(seq $count); do
+	    ./concurrent-sc-eb-stack -num_threads=$j -T=$1 -access_pattern=1 -avg_size=$2 >> ./output/sc-eb-stack$j ;
+	done
 done
--- experiments/experiments/concurrent/pattern-prob/start-sc-ts-hardware-stack-1.sh
+++ experiments/experiments/concurrent/pattern-prob/start-sc-ts-hardware-stack-1.sh
@@ -5,7 +5,12 @@
 
 cd ..; cd ..; cd ..; cd out/Debug;
 
-count=10
-for i in $(seq $count); do
-    ./concurrent-sc-ts-hardware-stack -num_threads=$1 -T=$2 -access_pattern=1 -avg_size=$3 >> ./output/sc-ts-hardware-stack$4 ;
+declare -a arr=(1 2 5 10 20 40 60 80)
+
+
+count=3
+for j in "${arr[@]}"; do
+	for i in $(seq $count); do
+	    ./concurrent-sc-ts-hardware-stack -num_threads=$j -T=$1 -access_pattern=1 -avg_size=$2 >> ./output/sc-ts-hardware-stack$j ;
+	done
 done
